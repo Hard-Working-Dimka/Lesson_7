2501636a25808a9d5e50f5c36c219037f8963794
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-10-16 21:24:24 +0300

    Cleaned the code

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv
 from pytimeparse import parse
 
 load_dotenv()
-# TODO: ПРОВЕРИТЬ PEP8 ПО ДЕВМАНУ + IF NAME == MAIN
 
-
-TG_TOKEN = os.getenv('TELEGRAM_TOKEN')  # подставьте свой ключ API
+TG_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
@@ -19,22 +17,24 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def notification_to_user(secs_left, user_id, message_id, message):
-    progressbar_message = render_progressbar(parse(message), parse(message) - secs_left)
+def notifications_to_user(secs_left, user_id, message_id, total_tima):
+    progressbar_message = render_progressbar(total_tima, total_tima - secs_left)
     bot.update_message(user_id, message_id, 'Осталось {0} секунд \n{1}'.format(secs_left, progressbar_message))
 
 
-def run_out(user_id):
+def timer_run_out(user_id):
     bot.send_message(user_id, 'Время вышло!')
 
 
-def start(user_id, message):
+def start(user_id, time_from_user):
+    total_tima = parse(time_from_user)
     message_id = bot.send_message(user_id, 'Запускаю таймер!')
-    bot.create_countdown(parse(message), notification_to_user, user_id=user_id, message_id=message_id, message=message)
-    bot.create_timer(parse(message), run_out, user_id=user_id)
+    bot.create_countdown(total_tima, notifications_to_user, user_id=user_id, message_id=message_id,
+                         total_tima=total_tima)
+    bot.create_timer(parse(time_from_user), timer_run_out, user_id=user_id)
 
 
-load_dotenv()
-bot = ptbot.Bot(TG_TOKEN)
-bot.reply_on_message(start)
-bot.run_bot()
+if __name__ == '__main__':
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(start)
+    bot.run_bot()
